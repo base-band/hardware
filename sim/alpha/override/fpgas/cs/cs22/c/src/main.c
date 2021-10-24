f59fd75c1776f06faa3b85a9f9f41a9486b48417
Ben <bjmorse@gmail.com>
2021-10-24 16:45:55 -0700

    Example of sending dma out, working but not flowing

--- sim/alpha/override/fpgas/cs/cs22/c/src/main.c
+++ sim/alpha/override/fpgas/cs/cs22/c/src/main.c
@@ -1,9 +1,17 @@
 #include "pass_fail.h"
+#include "dma.h"
+#include "vmem.h"
 
+VMEM_SECTION unsigned int test_buf[16] = {
+    0xf000e000,
+    0xf000e001,
+    0xf000e002,
+};
 
 int main(void)
 {
-    for(int i = 0; i < 1024; i++) {
-        *pass_fail_0 = i;
-    }
+    dma_out_set(VMEM_ROW_ADDRESS(test_buf), 16);
+    // for(int i = 0; i < 1024; i++) {
+    //     *pass_fail_0 = i;
+    // }
 }
